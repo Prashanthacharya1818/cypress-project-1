8634616f28dde1c84da1f303ecd10e35fb362630
Phashanthacharya1818 <Prashanthacharya0to9@gmail.com>
2023-08-22 18:00:28 +0530

    added login to github code.

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } =require('cypress')
 
 module.exports = {
-  projectId: 'gw7pna',
+ // projectId: 'gw7pna',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
---
+++ cypress/integration/examples/cypressdemo.spec.js
@@ -0,0 +1,15 @@
+describe('My First Test', () => {
+    it('Visits the facebook website', () => {
+      // open website HRMS
+        cy.visit('https://www.facebook.com/');
+
+        cy.get("#email").type("lrushikeswarreddy@gmail.com");
+
+        cy.get('#pass').type('Rushi@@21');
+
+        cy.wait(5000);
+
+        //cy.get('[type=submit]').click();
+        cy.contains('Log in').click();    
+      });
+});
